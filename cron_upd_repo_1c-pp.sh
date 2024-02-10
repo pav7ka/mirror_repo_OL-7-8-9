60994bb9aa4d1112b673d73d1cfe8ff3807f81e9
pav7ka <vorobjevpavel@gmail.com>
2024-02-10 22:42:34 +0300

    ..

---
+++ cron_upd_repo_1c-pp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export http_proxy="http://10.100.205.204:3128"
+export https_proxy="http://10.100.205.204:3128"
+
+P_REPO="/usr/share/nginx/html/repos"
+P_LOG="/root/mirror/log"
+
+for VER in 16 15 14 13 12 #11 10
+do
+    wget -c -np -nv --reject="index.html*" -e robots=off -m "https://repo.postgrespro.ru/1c-$VER/" -P "$P_REPO" &> "$P_LOG/postgrespro_1c-$VER__$(date +%Y-%m-%d-%k%M%S)"
+done
